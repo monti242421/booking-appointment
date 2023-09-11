14a0f47725b3bd50175e3063a5bfc125f2dc3a25
monti242421 <monti242421@gmail.com>
2023-09-11 18:52:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,11 +21,13 @@ var myobj = {
 
 var li =document.createElement("li");
 li.className="list-group-item";
-li.id=key;
 li.appendChild(document.createTextNode(Name +"-"+Email+"-"+PhoneNumber  ));
-axios.post("https://crudcrud.com/api/2c3bf3fea33c4aa385b87db9cd4d1517/appointmentapp",myobj).then((res)=>console.log(res) ).catch(err =>console.log(err));
-var myobjSerialized = JSON.stringify(myobj);
-localStorage.setItem(li.id,myobjSerialized);
+axios.post("https://crudcrud.com/api/ec0d28826ca04f71a15bc7e3a2921b78/appointmentapp",myobj)
+.then((res)=>{
+    li.id = res.data._id;
+    console.log(li.id);
+} )
+.catch(err =>console.log(err));
 
 var deletebtn = document.createElement("button");
 deletebtn.className="btn btn-danger btn-sm btn-space delete";
@@ -40,11 +42,48 @@ li.appendChild(editBtn);
 items.appendChild(li);
 }
 
+function showDataToScreen(data){
+    var Name = data.Name;
+    var Email = data.Email;
+    var PhoneNumber = data.PhoneNumber;
+    var li =document.createElement("li");
+    li.id = data._id;
+    li.className="list-group-item";
+    li.appendChild(document.createTextNode(Name +"-"+Email+"-"+PhoneNumber  ));
+    var deletebtn = document.createElement("button");
+    deletebtn.className="btn btn-danger btn-sm btn-space delete";
+    deletebtn.appendChild(document.createTextNode("Del"));
+
+    var editBtn = document.createElement("button");
+    editBtn.className="btn btn-primary btn-sm btn-space  edit ";
+    editBtn.appendChild(document.createTextNode("Edit"));
+
+    li.appendChild(deletebtn);
+    li.appendChild(editBtn);
+    items.appendChild(li);
+}
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get("https://crudcrud.com/api/ec0d28826ca04f71a15bc7e3a2921b78/appointmentapp")
+    .then((res)=>{
+        console.log(res.data)
+        for( var i=0;i<res.data.length;i++){
+            showDataToScreen(res.data[i]);
+        }
+    
+    }).catch(err=>console.log(err));
+
+
+})
+
 function removeItem(e){
     if(e.target.classList.contains("delete")){
         var li = e.target.parentElement;
+        console.log(li.id);
         items.removeChild(li);
-        localStorage.removeItem(li.id);
+        axios.delete("https://crudcrud.com/api/ec0d28826ca04f71a15bc7e3a2921b78/appointmentapp/"+li.id)
+        .then((res)=>{
+
+        }).catch(err=>console.log(err));
 
     }
     if(e.target.classList.contains("edit")){
@@ -56,7 +95,6 @@ function removeItem(e){
         document.getElementById("phonenumber").value= itemList[2];
 
         items.removeChild(li);
-        localStorage.removeItem(li.id);
     }
     
 }
